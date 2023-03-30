29b4893ad5ee56c6d284a89ddea359837c2c765d
diegobarreto18 <dbarreto9601@gmail.com>
2023-03-30 20:06:07 -0300

    Creo clases 30/03/2023

---
+++ define.h
@@ -0,0 +1,15 @@
+#define DEFINE_H
+#ifndef DEFINE_H
+
+// define.h
+// Modulo de Definiciones Generales.
+
+#define MAX_COMANDO 128
+#define MAX_NOMBRE 20
+
+
+enum _turno{Manana, Tarde, Noche};
+typedef enum _turno Turno;
+
+
+#endif
